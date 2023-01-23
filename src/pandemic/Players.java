a4436ad6a0cd3b482e0c7bf0369d83926f1976da
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a15p22.fil.univ-lille.fr>
2023-01-23 10:21:02 +0100

    MAJ

---
+++ src/pandemic/Players.java
@@ -0,0 +1,14 @@
+package pandemic;
+
+
+public class Players{
+	private name String;
+	private role Roles;
+	private location Cities;
+	
+	public Players(name String, role Roles, location Cities) {
+		
+		
+	}
+	
+}
