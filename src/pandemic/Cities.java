3d1fcdd83d412b00b9f1196b1738b8c6933a455f
Benikken <anais.benikken.etu@univ-lille.fr>
2023-01-24 12:00:17 +0100

    mise à jour de la classe Cities

--- src/pandemic/Cities.java
+++ src/pandemic/Cities.java
@@ -1,6 +1,31 @@
 package pandemic;
+import java.util.*;
 
 public class Cities {
 	protected String name ;
+	protected List<Cubes> infectionCubes;
+	protected List<Cities> neighbors;
 	
+	
+	public Cities(String name ) {
+		this.name=name;
+	}
+	public boolean isInfectionFocus() {
+		if (InfectionFocusAmount==3) {
+			return true;
+		}
+		return false;
+	}
+	public String getName() {
+		return this.name;
+	}
+	 public List<Cubes> getAllCubes() {
+	      return this.infectionCubes;
+	   }
+	 public List<Cities> getAllCities() {
+	      return this.neighbors;
+	   }
+	public String toString() {
+		return "this city's name is " + this.name ;
+	}
 }
