b675c4c8e2a6b8008ba3b67d275930296a2bd538
elala <elala@10.21.5.234>
2023-02-06 08:10:07 +0100

    Ajout du package de test junit

---
+++ test/pandemic/CitiesTest.java
@@ -0,0 +1,14 @@
+package pandemic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CitiesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
