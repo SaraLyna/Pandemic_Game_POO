fa2ee952ac30670634cb42547a44a3215a9db701
elala <elala@192.168.1.82>
2023-02-23 08:25:03 +0100

    classe test pour la map monde

--- test/pandemic/MappeMondeTest.java
+++ test/pandemic/MappeMondeTest.java
@@ -1,14 +1,22 @@
 package pandemic;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import java.io.FileNotFoundException;
 
 public class MappeMondeTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+	/**
+     * Teste la création d'une carte du monde à partir d'un fichier JSON, l'affichage de toutes les villes et 
+     * la fin du tour des épidémies.
+     * @throws FileNotFoundException si le fichier JSON spécifié n'est pas trouvé
+     */
+    @Test
+    public void testMappeMonde() {
+        try {
+            MappeMonde map = new MappeMonde("villes.json");
+            map.displayAllCities();
+            map.endInfectionTurn();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
