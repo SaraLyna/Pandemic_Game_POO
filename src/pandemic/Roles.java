f826a66dd73e36deddf346816b4a288b3537919d
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a15p3.fil.univ-lille.fr>
2023-03-06 08:27:36 +0100

    ajout des classes pour chaque role

--- src/pandemic/Roles.java
+++ src/pandemic/Roles.java
@@ -1,8 +1,8 @@
 package pandemic;
 
 	/**
+	 * this abstract class is for definition of the roles of each player
 	 * 
-	 *
 	 */
 	public abstract class Roles{
 		//on devra créer les classes des roles aprés
