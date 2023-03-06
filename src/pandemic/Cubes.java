73ebf2968578a904e84d5665ce8d3fe4420e87dc
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a15p3.fil.univ-lille.fr>
2023-03-06 09:40:50 +0100

    Merge branch 'main' of git@gitlab-etu.fil.univ-lille.fr:saralyna.ouyahia.etu/l-2-s-4-projet-2023-groupe-8.git into main

--- src/pandemic/Cubes.java
+++ src/pandemic/Cubes.java
@@ -10,16 +10,25 @@ public class Cubes {
 	
 	
 	
+	/**
+	 * @param diseasename
+	 */
 	public Cubes (String diseasename) {
 		this.diseasename=diseasename;
 	}
 	
+	/**
+	 * @return the name of the disease
+	 */
 	public String getDiseaseName() {
 		return this.diseasename;
 	}
 	
 	
    
+	/**
+	 *Returns a string representation of the object.
+	 */
 	public String toString() {
 		return this.diseasename;
 		
