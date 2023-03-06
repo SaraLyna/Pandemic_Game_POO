37cd8d1f2656fe8a519373acbea75719a981aea2
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a15p3.fil.univ-lille.fr>
2023-03-06 09:37:53 +0100

    enum class for Roles and extends for Actions

---
+++ src/pandemic/GlobetRotter.java
@@ -0,0 +1,16 @@
+package pandemic;
+
+/**
+ * this class represent the action of the GlobetRotter
+ *
+ */
+
+class GlobetRotter extends Actions {
+	//en relation avec l'action se déplacer
+		//a la possibilité de se déplacer dans n’importe quelle ville. Ce qui augmente sa capacité d’action. 
+		private String name;
+		public String getName() {
+			return this.name;
+		}
+
+}
