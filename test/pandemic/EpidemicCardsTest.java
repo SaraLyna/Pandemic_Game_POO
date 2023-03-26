401aa1b1289b6c85a41ca5f96659a803539a8b3d
Sara Lyna OUYAHIA <saralyna2002@gmail.com>
2023-03-26 04:27:00 +0200

    test

---
+++ test/pandemic/EpidemicCardsTest.java
@@ -0,0 +1,14 @@
+package pandemic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EpidemicCardsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
