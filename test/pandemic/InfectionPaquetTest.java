ab87de7011ad8e99917743ee14acd0110e103d79
Benikken <anais.benikken.etu@univ-lille.fr>
2023-03-28 10:49:27 +0200

    implémentation des tests pour la classe InfectionPaquet

--- test/pandemic/InfectionPaquetTest.java
+++ test/pandemic/InfectionPaquetTest.java
@@ -1,21 +1,53 @@
-/**
- * 
- */
 package pandemic;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * @author Sara Lyna
- *
- */
+
 public class InfectionPaquetTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
+private InfectionPaquet paquet;
+    
+    @Test
+    public void testInitCards() {
+    	InfectionPaquet paquet= new InfectionPaquet();
+        assertEquals(48, paquet.nbCartes());
+    }
+
+    @Test
+    public void testAddCarte() {
+    	InfectionPaquet paquet= new InfectionPaquet();
+        paquet.addCarte(new InfectionCards("ville-50", "bleu"));
+        assertEquals(49, paquet.nbCartes());
+    }
+
+    @Test
+    public void testMelanger() {
+    	InfectionPaquet paquet= new InfectionPaquet();
+    	// Vérifier que les cartes sont mélangées
+    	
+        InfectionCards carte1 = paquet.tirerCarte();
+        InfectionCards carte2 = paquet.tirerCarte();
+        assertNotEquals(carte1, carte2);
+    }
+
+    @Test
+    public void testTirerCarte() {
+    	InfectionPaquet paquet= new InfectionPaquet();
+        InfectionCards carte = paquet.tirerCarte();
+        assertNotNull(carte);
+        assertEquals(47, paquet.nbCartes());
+    }
+    @Test
+    public void testNbCartes() {
+    	InfectionPaquet paquet= new InfectionPaquet();
+        assertEquals(48, paquet.nbCartes());
+        paquet.tirerCarte();
+        assertEquals(47, paquet.nbCartes());
+    }
+    
+    
 
 }
