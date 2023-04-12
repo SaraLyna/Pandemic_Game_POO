6c6102fb14552eaff581a820f7debcf2f53efbfc
Clement Glm <clement.guillaumin.etu@univ-lille.fr>
2023-04-12 11:36:59 +0200

    moveTest() rédigée

--- test/pandemic/GlobetRotterTest.java
+++ test/pandemic/GlobetRotterTest.java
@@ -2,7 +2,10 @@ package pandemic;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+//import org.junit.Test;
 
 public class GlobetRotterTest {
 	
@@ -10,6 +13,8 @@ public class GlobetRotterTest {
 	private Cities city1;
 	private Cities city2;
 	
+	
+	/*
 	@Test 
 	public void MoveAnywhereTest(){
 		
@@ -20,6 +25,27 @@ public class GlobetRotterTest {
 		assertFalse(player.getLocation().getName().equals(city1.getName()));
 		assertTrue(player.getLocation().getName().equals(city2.getName()));
 		
+	}
+	*/
+	
+	public void moveTest() { 
+		MappeMonde m1 = new MappeMonde("data/carte.json");
+		city1 = m1.getVilles()[2];
+		player = new Players("Albert",Roles.GlobetRotter, city1 );
+		
+		city2 = city1.getNeighbors().get(0); //premier voisin listé
+		
+		assertTrue(player.getLocation().getName().equals(city1.getName()));
+		
+		//simulation de l'utilisateur qui prends le 1er choix, pour les détails, cf https://stackoverflow.com/questions/31635698/junit-testing-for-user-input-using-scanner
+		String input = "1";
+		InputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+		
+		GlobetRotter.move(player, m1); //todo : voir si globetrotter est statique ou non
+		
+		assertFalse(player.getLocation().getName().equals(city1.getName()));
+		assertTrue(player.getLocation().getName().equals(city2.getName()));
 	}
 
 	}
